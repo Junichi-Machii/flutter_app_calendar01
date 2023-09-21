cc8fcd8477cf7f27bcd2a7cf5aecf386c6232f35
Junichi-Machii <junichi878338@hotmail.co.jp>
2023-09-21 15:37:16 +0900

    calendar Complete

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_cakendar_app01/views/event_calendar.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
+void main() {
+  initializeDateFormatting().then((_) => runApp(MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          brightness: Brightness.dark,
+          primaryColor: const Color.fromARGB(255, 96, 139, 121)),
+      home: EventCalendarScreen(),
+    );
+  }
+}
